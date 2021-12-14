de14db64293a15b62e75cb048e3f5e7a6633bf73
SpaceWolfy <s_Scozzari@libero.it>
2021-12-14 15:22:01 +0100

    javascript structure completed

--- js/script.js
+++ js/script.js
@@ -12,3 +12,44 @@ const paragraphSwitch = [
     'Volete preparare un aperitivo insolito? Lasciate stare le patatine e preparate della chips di barbabietola, da servire con noccioline e olive. Tagliate la barbabietola cruda in fettine molto sottili, buttatele nell’olio caldo e lasciate cuocere sino a che non diventeranno croccanti. Asciugate le chips con della carta assorbente e servite caldissime.',
     'Il termine simmetria indica generalmente la presenza di alcune ripetizioni nella forma geometrica di un oggetto.'
 ];
+
+//Creo la variabile currentIndex, in let, per far sì che possa variare
+//questa variabile andrà a lavorare sia per il primo array che per il secondo.
+let currentIndex = 0;
+
+//Creo due variabili da correlare a titoli e paragrafi:
+let h2Title = document.querySelector('.paragraph-title');
+let paragraph = document.querySelector('.paragraph');
+
+//Definisco le variabili relative ai pulsanti:
+let btnBackward = document.querySelector('.fa-arrow-circle-left');
+let btnForward = document.querySelector('.fa-arrow-circle-right');
+
+//associazione variabili:
+/* h2Title = titleSwitch[currentIndex];
+paragraph = paragraphSwitch[currentIndex]; */
+
+//output
+h2Title.innerHTML = `<h2>${titleSwitch[currentIndex]}</h2>`
+paragraph.innerHTML = `<p>${paragraphSwitch[currentIndex]}</p>`
+
+//funzione legata al click della freccia destra (avanti)
+btnForward.addEventListener('click', function(){
+    currentIndex++
+    if(currentIndex > titleSwitch.length -1 && currentIndex > paragraphSwitch.length - 1) {
+        currentIndex = 0;
+    }
+    h2Title.innerHTML = `<h2>${titleSwitch[currentIndex]}</h2>`
+    paragraph.innerHTML = `<p>${paragraphSwitch[currentIndex]}</p>`
+});
+
+//funzione legata al click della freccia sinistra (indietro)
+btnBackward.addEventListener('click', function(){
+    currentIndex--
+    if(currentIndex === -1) {
+        currentIndex = 3;
+    }
+    console.log(currentIndex)
+    h2Title.innerHTML = `<h2>${titleSwitch[currentIndex]}</h2>`
+    paragraph.innerHTML = `<p>${paragraphSwitch[currentIndex]}</p>`
+});
