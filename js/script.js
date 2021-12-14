4ec36710cfc810ea82ec6cf87f9829c705e7d89c
SpaceWolfy <s_Scozzari@libero.it>
2021-12-14 14:22:13 +0100

    the two main arrays have been created

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Creazione degli array
+const titleSwitch = [
+    'Definizione di universo', 
+    'Il mito della biga alata', 
+    'Come cucinare la barbabietola', 
+    'La simmetria'
+];
+
+const paragraphSwitch = [
+    'L’Universo si può definire come “l’insieme di tutte le cose che esistono”: stelle, galassie, pianeti, materia, energia. Ad occhio nudo è visibile solo una piccolissima parte di quello che noi definiamo Universo.',
+    'Il mito del carro e dell\'auriga, o della biga alata, tratto dal Fedro di Platone, serve a spiegare la teoria platonica della reminiscenza dell\'anima, un fenomeno che durante la reincarnazione produce ricordi legati alla vita precedente.',
+    'Volete preparare un aperitivo insolito? Lasciate stare le patatine e preparate della chips di barbabietola, da servire con noccioline e olive. Tagliate la barbabietola cruda in fettine molto sottili, buttatele nell’olio caldo e lasciate cuocere sino a che non diventeranno croccanti. Asciugate le chips con della carta assorbente e servite caldissime.',
+    'Il termine simmetria indica generalmente la presenza di alcune ripetizioni nella forma geometrica di un oggetto.'
+];
